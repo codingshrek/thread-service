5c3b042f69628a9bf52cd26e02a3b84ce86caa02
Akash Yadav <akash.insignia@gmail.com>
2026-08-02 02:13:04 +0530

    Initial commit: thread-service boilerplate

---
+++ src/test/java/com/paddock/thread/ThreadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paddock.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
